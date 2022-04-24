ed6e5ebe2dcc4a5d63440dd7f208c28b8d98f518
RVILLANUEVA VILLANUEVA <103458775+rvillanueva3@users.noreply.github.com>
2022-04-24 19:30:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+
+int number;
+    int = 25
+      
+System.out.println (number)
+
+System.out.println("Data Types");
+
+  }
+}
---
+++ data-types/DataTypes.java
@@ -0,0 +1,38 @@
+class DataTypes {
+   public static void main(String[]args){
+
+  String day = "Sunday";
+  String day2 = "Monday";
+  String month = "April";
+  String comma  = ",";
+  String space = " ";
+  String period = ".";
+  int date =  24;
+  int date2 = 25;
+  int year =  2022;
+  
+  
+  
+  System.out.print("Today's date is ");
+  System.out.print(day);
+  System.out.print(space);  
+  System.out.print(month);
+  System.out.print(space); 
+  System.out.print(date);
+  System.out.print(comma);
+  System.out.print(year);
+  System.out.print(period); 
+  System.out.print(space); 
+  System.out.print("Tomorrow will be ");
+  System.out.print(day2);
+  System.out.print(space);  
+  System.out.print(month);
+  System.out.print(space); 
+  System.out.print(date2);
+  System.out.print(comma);
+  System.out.print(year);
+  System.out.print(period);    
+}
+
+  
+}
